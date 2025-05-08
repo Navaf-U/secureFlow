1af5d2fda748cd50196e081d405a23d000d488cf
navaf <navafsoft@gmail.com>
2025-05-08 19:16:02 +0530

    add dotenv package for environment variable management

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,9 +1,15 @@
 import express from 'express';
+import dotenv from 'dotenv';
+import { connect } from 'http2';
+import connectDB from './config/connectDB';
+dotenv.config();
 
 const app = express();
 const PORT = process.env.PORT || 5000;
 
 app.use(express.json());
+
+connectDB();
 
 app.get('/', (_req, res) => {
   res.send('DealVault server is running!');
