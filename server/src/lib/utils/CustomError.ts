d194493d67b910fce249f2b6aa4874451cdccb8b
navaf <navafsoft@gmail.com>
2025-05-10 18:57:28 +0530

    style: improve code formatting in CustomError and tryCatch utilities

--- server/src/lib/utils/CustomError.ts
+++ server/src/lib/utils/CustomError.ts
@@ -1,9 +1,9 @@
 export default class CustomError extends Error {
-    statusCode: number;
-    data?: unknown;
-    constructor(statusCode: number, message: string, data?: unknown) {
-      super(message);
-      this.statusCode = statusCode;
-      this.data = data;
-    }
+  statusCode: number;
+  data?: unknown;
+  constructor(statusCode: number, message: string, data?: unknown) {
+    super(message);
+    this.statusCode = statusCode;
+    this.data = data;
   }
+}
