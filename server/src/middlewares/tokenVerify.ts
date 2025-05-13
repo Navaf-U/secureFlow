2ceb9446c652fe8a23539bef752ec6b00c05bd62
navaf <navafsoft@gmail.com>
2025-05-13 15:04:57 +0530

    feat: add token verification middleware with authenticated request type

---
+++ server/src/middlewares/tokenVerify.ts
@@ -0,0 +1,34 @@
+import {  Response, NextFunction } from "express";
+import { AuthenticatedRequest } from "../lib/types/type";
+import jwt from "jsonwebtoken";
+import  CustomError  from "../lib/utils/CustomError";
+
+
+const tokenVerify = (
+  req: AuthenticatedRequest,
+  res: Response,
+  next: NextFunction
+) => {
+  try {
+    const authHeader = req.headers.authorization;
+    if (!authHeader) {
+      return next(new CustomError(401, "You are not authenticated"));
+    }
+
+    const token = authHeader.split(" ")[1];
+
+    jwt.verify(token, process.env.JWT_TOKEN as string, (err: Error | null, decoded: jwt.JwtPayload | string | undefined) => {
+      if (err) {
+        console.error("JWT verification error:", err.message);
+        return next(new CustomError(401, "Token is not valid"));
+      }
+      const { userId } = decoded as { userId: string };
+      req.user = userId;
+      next();
+    });
+  } catch (error) {
+    next(new CustomError(401, "Failed to verify authentication"));
+  }
+};
+
+export default tokenVerify;
